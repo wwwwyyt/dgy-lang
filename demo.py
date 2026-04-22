8aa3c3790bedb5a6cc82f8a6cb4c8e49317a6ba6
wwwwyyt <wwwwyyt@126.com>
2026-04-22 17:28:05 +0800

    add demo.py

---
+++ demo.py
@@ -0,0 +1,12 @@
+def lexer(src: str) -> list:
+    symble_table = []
+    
+    return symble_table
+
+
+if __name__ == "__main__":
+    src = '''
+    存 `你` 到 1 号
+    '''
+    symble_table = lexer(src)
+    print(symble_table)
